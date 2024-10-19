2d4166654f771584b6c30900fc13df5c52b2d009
BKDR01 <bahodirov.bobur@icloud.com>
2024-10-19 18:40:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let type1 = prompt('String typedagi malumotingizni kiriting')
+let type2 = prompt('Number typedagi malumotingizni kiriting')
+let type3 = prompt('boolean typedagi malumotingizni kiriting')
+
+confirm('Hamma malumotlarni togri kiritganizga ishonchingiz komilmi?')
+let ns = alert(`String typedagi malumotingiz ${type1} \n Number typedagi malumotingiz ${type2} \n boolean typedagi malumotingiz ${type3}`)
+console.log(ns);
+
